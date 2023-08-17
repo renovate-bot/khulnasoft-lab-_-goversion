f5c125a0881494c734619cf8f0fd32bb0d832b6e
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-08-17 22:42:23 +0600

    Update prerelease_test.go

--- pkg/prerelease/prerelease_test.go
+++ pkg/prerelease/prerelease_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/khulnasoft-labs/goversion/pkg/part"
+	"github.com/khulnasoft-lab/goversion/pkg/part"
 )
 
 func TestCompare(t *testing.T) {
